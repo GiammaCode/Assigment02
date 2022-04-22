80e6c223ae0f9284763d466dcf60f4c2acbc9ec1
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2022-04-22 17:26:10 +0200

    add pir class

--- src/Components/Pir.h
+++ src/Components/Pir.h
@@ -1,6 +1,25 @@
 #ifndef __PIR__
 #define __PIR__
 
+#include <Arduino.h>
+
+class Pir
+{
+private:
+    byte pin;
+    byte state;
+    byte laststate;
+
+public:
+    Pir(byte pin);
+
+    void init();
+    void update();
+
+    byte getState();
+    bool isDetected();
+   
+};
 
 
 
