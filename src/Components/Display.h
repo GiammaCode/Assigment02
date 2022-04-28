7381234f1633643f8258df20dc18e29bc48167b1
Luca <luca.pasini8@studio.unibo.it>
2022-04-28 10:11:00 +0200

    Dsiplay.h

--- src/Components/Display.h
+++ src/Components/Display.h
@@ -1,11 +1,26 @@
 #ifndef __DIPLAY__
 #define __DISPLAY__
+#include <Arduino.h>
+
+
+class Display {
+
+private:
+    byte sda;
+    byte scl;
+
+public: 
+    Display();
+
+    Display (byte sda, byte scl);
+
+    void print();
+
+    void init();
 
 
 
 
 
-
-
-
+};
 #endif
