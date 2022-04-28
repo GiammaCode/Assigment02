be733f8d8385a719db860d4ee8d2419c2f06e392
LoryBug <72506393+LoryBug@users.noreply.github.com>
2022-04-28 10:24:24 +0200

    add l2c lib

--- src/Welcome.h
+++ src/Welcome.h
@@ -1,11 +1,16 @@
-
+#include "LiquidCrystal_I2C.h"
 #include "Task.h"
+#include "Arduino.h"
+
 
 class Welcome: public Task {
   
   int pin;
 
-    void tick();
+    void tick(
+      
+    );
     void init(int pertiod);
+
     
 };
