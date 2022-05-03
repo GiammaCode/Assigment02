b72b39c008f3b879b438730437b1d9f72fbb8a38
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2022-05-03 16:06:25 +0200

    test sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,41 @@
 #include <Arduino.h>
 #include <./Task/Welcome.h>
 #include <Wire.h>
+#include <stdio.h>
 
 #include <./Components/Pir.h> 
+#include <./Components/ServoMotorImpl.h>
+#include <./Components/Sonar.h>
+#include <./Components/Display.h> 
+#include <Servo.h>
+
+
 
 Task* t0 = new Welcome();
 Pir* p = new Pir(2);
+ServoMotor* motor = new ServoMotorImpl(3);
+Sonar* sonar = new Sonar(4,5);
+
+int pos = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
-  //t0->init(3000);
-  pinMode(13, OUTPUT);
-  
- 
+  t0->init(3000);
+  motor->on();  
+  sonar->init();
+  Serial.begin(9600);
+  Serial.println("init");
+   
 }
   
 
 
 void loop() {
   // put your main code here, to run repeatedly:
- // t0->tick();
- if(p->isDetected()){
-   digitalWrite(13, HIGH); 
- }
- else{
-   digitalWrite(13, LOW); 
- }
+  t0->tick();
+  Serial.print("dist:  ");
+  Serial.println(sonar->getDistance());
+
   
 }
